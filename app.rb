9ed0d6d1cdb93f748dc531d5f5c43d95135905ed
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-04 03:39:17 +0000

    first round - home page with currency list

--- app.rb
+++ app.rb
@@ -1,9 +1,51 @@
+
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+require "http"
+require "json"
 
+exchange_rate_base_url = "https://api.exchangerate.host"
+exchange_rate_key = ENV.fetch("EXCHANGE_RATE_KEY")
+exchange_rate_url =  "https://api.exchangerate.host/list?access_key=#{exchange_rate_key}"
+
+# raw_exchange_rate_url = HTTP.get(exchange_rate_url)
+# parsed_exchange_rate = JSON.parse(raw_exchange_rate_url)
+
+# check_success = parsed_exchange_rate.fetch("success")
+# pp check_success.class
+# @currencies = parsed_exchange_rate.fetch["currencies"]
+
+
+#Homepage - List of Currencies
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  raw_exchange_rate_url = HTTP.get(exchange_rate_url)
+  parsed_exchange_rate = JSON.parse(raw_exchange_rate_url)
+
+  if parsed_exchange_rate.fetch("success") == true
+    @currencies = parsed_exchange_rate.fetch("currencies")
+  else
+    @currencies = []
+  end
+
+  erb :index
 end
+
+# # Conversion Page
+# get("/:currency") do
+#   @currency = params[:currency]
+
+#   uri = URI("#{API_BASE_URL}/latest?base=#{@currency}")
+#   response = Net::HTTP.get(uri)
+#   data = JSON.parse(response)
+
+#   puts data
+
+#   if data["success"]
+#     @rates = data["rates"]
+#   else
+#     @rates = {}
+#   end
+
+#   erb :convert
+# end
