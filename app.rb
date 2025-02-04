e0167236e96ec883b339d9b81b364ed2119a2d31
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-04 20:23:07 +0000

    final updates

--- app.rb
+++ app.rb
@@ -9,12 +9,15 @@ exchange_rate_base_url = "https://api.exchangerate.host"
 exchange_rate_key = ENV.fetch("EXCHANGE_RATE_KEY")
 exchange_rate_url =  "https://api.exchangerate.host/list?access_key=#{exchange_rate_key}"
 
-# raw_exchange_rate_url = HTTP.get(exchange_rate_url)
-# parsed_exchange_rate = JSON.parse(raw_exchange_rate_url)
 
-# check_success = parsed_exchange_rate.fetch("success")
-# pp check_success.class
-# @currencies = parsed_exchange_rate.fetch["currencies"]
+
+currency_conversion_rate_url = "https://api.exchangerate.host/convert?from=USD&to=INR&amount=1&access_key=#{exchange_rate_key}"
+raw_currency_conversion_rate = HTTP.get(currency_conversion_rate_url)
+parsed_conversion_data = JSON.parse(raw_currency_conversion_rate)
+
+pp parsed_conversion_data  
+
+
 
 
 #Homepage - List of Currencies
@@ -28,24 +31,40 @@ get("/") do
     @currencies = []
   end
 
-  erb :index
+  erb( :index, {:layout => :layout}) 
 end
 
-# # Conversion Page
-# get("/:currency") do
-#   @currency = params[:currency]
+# Convert Currency Page
+ get("/:currency") do
+   currency = params[:currency]
 
-#   uri = URI("#{API_BASE_URL}/latest?base=#{@currency}")
-#   response = Net::HTTP.get(uri)
-#   data = JSON.parse(response)
+  raw_exchange_rate_url = HTTP.get(exchange_rate_url)
+  parsed_exchange_rate = JSON.parse(raw_exchange_rate_url)
 
-#   puts data
+  if parsed_exchange_rate.fetch("success") == true
+    @currencies = parsed_exchange_rate.fetch("currencies")
+  else
+    @currencies = []
+  end
 
-#   if data["success"]
-#     @rates = data["rates"]
-#   else
-#     @rates = {}
-#   end
+  erb( :convert_currency, {:locals => {:currency => currency} }, {:layout => :layout} )
+end
 
-#   erb :convert
-# end
+#Currency to Currency Conversion Page
+get("/:currency_1/:currency_2") do
+  currency_1 = params[:currency_1]
+  currency_2 = params[:currency_2]
+
+  currency_conversion_rate_url = "https://api.exchangerate.host/convert?from=#{currency_1}&to=#{currency_2}&amount=1&access_key=#{exchange_rate_key}"
+  raw_currency_conversion_rate = HTTP.get(currency_conversion_rate_url)
+  parsed_conversion_data = JSON.parse(raw_currency_conversion_rate)
+
+  if parsed_conversion_data.fetch("success") == true
+    rate = parsed_conversion_data.fetch("result")
+  else
+    rate = nil
+  end
+
+  erb(:conversion, { :locals => {:currency_1 => currency_1, :currency_2 => currency_2, :rate => rate}}, {:layout => :layout})
+
+end
